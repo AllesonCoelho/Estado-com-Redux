8d8e2577538de7c375a7473e7c44b2e747d866af
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-01 21:06:01 -0300

    Componente Intervalo

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from '../src/components/Card'
+import Intervalo from '../src/components/Intervalo'
 
 function App() {
   return (
     <div className="App">
       <h1>Exercicios React-Redux</h1>
       <div className="linha">
-        <Card title="Card 1" red>
-          x
-        </Card>
+       <Intervalo></Intervalo>
+      </div>
+      <div className="linha">
         <Card title="Card 1" green>
           x
         </Card>
-      </div>
-      <div className="linha">
         <Card title="Card 2" blue>
           y
       </Card>
 
-      <Card title="Card 2" purple>
+        <Card title="Card 2" purple>
           y
       </Card>
       </div>
