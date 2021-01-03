689c9828f6aedefc6059b36b9dec2071c4459b34
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-03 18:35:43 -0300

    Constantes para os tipos das Actions

--- redux-simples/src/store/actions/numeros.js
+++ redux-simples/src/store/actions/numeros.js
@@ -1,13 +1,14 @@
+import {NUM_MAX_ALTERADO, NUM_MIN_ALTERADO} from '../actions/actionsTypes'
 export function alterarNumeroMin(novoNumero){
     return {
-        type: 'NUM_MIN_ALTERADO',
+        type: NUM_MIN_ALTERADO,
         payload: novoNumero
     }
 }
 
 export function alterarNumeroMax(novoNumero){
     return {
-        type: 'NUM_MAX_ALTERADO',
+        type: NUM_MAX_ALTERADO,
         payload: novoNumero
     }
 }
--- redux-simples/src/store/reducers/numeroReducer.js
+++ redux-simples/src/store/reducers/numeroReducer.js
@@ -1,3 +1,5 @@
+import {NUM_MAX_ALTERADO, NUM_MIN_ALTERADO} from '../actions/actionsTypes'
+
 const initialState = {
     min: 7,
     max: 31
@@ -6,12 +8,12 @@ const initialState = {
 
 export default function (state = initialState, action) {
     switch (action.type) {
-        case 'NUM_MIN_ALTERADO':
+        case NUM_MIN_ALTERADO:
             return {
                 ...state,
                 min: action.payload
             }
-        case 'NUM_MAX_ALTERADO':
+        case NUM_MAX_ALTERADO:
             return {
                 ...state,
                 max: action.payload
